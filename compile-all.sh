5a5c220c3040b5a2727a5a5af93075d5c95ada8b
Chun Shen <chunshen1987@gmail.com>
2014-06-29 13:30:18 -0400

    add compile all script for compile programs at first time

---
+++ compile-all.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+for ii in VISHNew iSS iS osc2u urqmd
+    do
+    echo "Compiling " $ii " ..."
+    (cd $ii 
+     make distclean
+     make
+     make clean
+    )
+done
+
+echo "All programs have been compiled. Now let fun begins!"
