1e0b7f35d81f5baa2fadd4ff79e9b5af6648d4f3
Chun Shen <chunshen1987@gmail.com>
2014-07-03 09:02:50 -0400

    forgot to change the color back

--- compile-all.sh
+++ compile-all.sh
@@ -10,4 +10,4 @@ for ii in VISHNew iSS iS osc2u urqmd
     )
 done
 
-echo -e "\033[1;32m All programs have been compiled. Now let the fun begin!"
+echo -e "\033[1;32m All programs have been compiled. Now let the fun begin! \033[0m"
